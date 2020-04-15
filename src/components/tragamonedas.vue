4d240ebfded3e578cab36bc58453746d0eacafd7
Roberto Torres <roberto@Robertos-MacBook-Pro.local>
2020-04-16 00:36:45 +0100

    removing data from tragamonedas component

--- src/components/tragamonedas.vue
+++ src/components/tragamonedas.vue
@@ -78,30 +78,9 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { TimelineMax, Bounce } from 'gsap'
-import {
-  faBiohazard,
-  faBrain,
-  faGem,
-  faHandHoldingUsd,
-  faHatWizard,
-  faHeart,
-  faHouseDamage,
-  faJedi,
-  faLeaf,
-  faMask,
-  faMedal,
-  faMinus,
-  faPeace,
-  faPooStorm,
-  faPray,
-  faRadiation,
-  faSave,
-  faSpaceShuttle,
-  faSkullCrossbones,
-  faUserAstronaut
-} from '@fortawesome/free-solid-svg-icons'
 import Prize from '@/components/prize'
 import Reel from '@/components/reel'
 import Rule from '@/components/rule'
@@ -120,103 +99,17 @@ export default {
   },
 
   data() {
-    return {
-      dash: faMinus,
-      icons: [
-        { icon: faBrain, desc: 'brain' },
-        { icon: faGem, desc: 'diamond' },
-        { icon: faMask, desc: 'double-faced' },
-        { icon: faLeaf, desc: 'eco' },
-        { icon: faSkullCrossbones, desc: 'evil' },
-        { icon: faPeace, desc: 'good' },
-        { icon: faSpaceShuttle, desc: 'high tech' },
-        { icon: faUserAstronaut, desc: 'highly skilled' },
-        { icon: faHouseDamage, desc: 'home damage' },
-        { icon: faJedi, desc: 'jedi' },
-        { icon: faSave, desc: 'legacy' },
-        { icon: faHeart, desc: 'love' },
-        { icon: faHandHoldingUsd, desc: 'money' },
-        { icon: faRadiation, desc: 'poluter' },
-        { icon: faPooStorm, desc: 'shit storm' },
-        { icon: faMedal, desc: 'recognition' },
-        { icon: faPray, desc: 'suffer' },
-        { icon: faBiohazard, desc: 'toxic' },
-        { icon: faHatWizard, desc: 'wizard' }
-      ],
-      company: [
-        faGem,
-        faHeart,
-        faMedal,
-        faHandHoldingUsd,
-        faPeace,
-        faSpaceShuttle,
-        faLeaf,
-        faPooStorm,
-        faRadiation,
-        faSkullCrossbones
-      ],
-      team: [
-        faGem,
-        faHeart,
-        faMedal,
-        faHandHoldingUsd,
-        faHatWizard,
-        faUserAstronaut,
-        faJedi,
-        faPooStorm,
-        faMask,
-        faBiohazard
-      ],
-      job: [
-        faGem,
-        faHeart,
-        faMedal,
-        faHandHoldingUsd,
-        faBrain,
-        faUserAstronaut,
-        faSave,
-        faPooStorm,
-        faPray,
-        faHouseDamage
-      ],
-      prizes: [
-        {
-          combination: [faGem, faGem, faGem],
-          prize: "1'000'000",
-          note: 'WIN WIN WIN'
-        },
-        {
-          combination: [faHeart, faHeart, faHeart],
-          prize: "729'000",
-          note: 'WIN WIN WIN'
-        },
-        {
-          combination: [faMedal, faMedal, faMedal],
-          prize: "512'000",
-          note: 'WIN WIN'
-        },
-        {
-          combination: [faHandHoldingUsd, faHandHoldingUsd, faHandHoldingUsd],
-          prize: "512'000",
-          note: 'WIN'
-        },
-        {
-          combination: [faPeace, faHatWizard, faBrain],
-          prize: "512'000",
-          note: 'WIN WIN'
-        },
-        {
-          combination: [faSpaceShuttle, faUserAstronaut, faUserAstronaut],
-          prize: "512'000",
-          note: 'WIN WIN'
-        },
-        {
-          combination: [faLeaf, faJedi, faBrain],
-          prize: "512'000",
-          note: 'WIN WIN'
-        }
-      ]
-    }
+    return {}
+  },
+
+  computed: {
+    ...mapGetters({
+      icons: 'game/getIcons',
+      company: 'game/getCompany',
+      team: 'game/getTeam',
+      job: 'game/getJob',
+      prizes: 'game/getPrizes'
+    })
   },
 
   mounted() {},
---
+++ src/store/game.js
@@ -0,0 +1,143 @@
+import {
+  faBiohazard,
+  faBrain,
+  faGem,
+  faHandHoldingUsd,
+  faHatWizard,
+  faHeart,
+  faHouseDamage,
+  faJedi,
+  faLeaf,
+  faMask,
+  faMedal,
+  faPeace,
+  faPooStorm,
+  faPray,
+  faRadiation,
+  faSave,
+  faSpaceShuttle,
+  faSkullCrossbones,
+  faUserAstronaut
+} from '@fortawesome/free-solid-svg-icons'
+
+export const state = () => ({
+  counter: 0,
+  icons: [
+    { icon: faBrain, desc: 'brain' },
+    { icon: faGem, desc: 'diamond' },
+    { icon: faMask, desc: 'double-faced' },
+    { icon: faLeaf, desc: 'eco' },
+    { icon: faSkullCrossbones, desc: 'evil' },
+    { icon: faPeace, desc: 'good' },
+    { icon: faSpaceShuttle, desc: 'high tech' },
+    { icon: faUserAstronaut, desc: 'highly skilled' },
+    { icon: faHouseDamage, desc: 'home damage' },
+    { icon: faJedi, desc: 'jedi' },
+    { icon: faSave, desc: 'legacy' },
+    { icon: faHeart, desc: 'love' },
+    { icon: faHandHoldingUsd, desc: 'money' },
+    { icon: faRadiation, desc: 'poluter' },
+    { icon: faPooStorm, desc: 'shit storm' },
+    { icon: faMedal, desc: 'recognition' },
+    { icon: faPray, desc: 'suffer' },
+    { icon: faBiohazard, desc: 'toxic' },
+    { icon: faHatWizard, desc: 'wizard' }
+  ],
+  company: [
+    faGem,
+    faHeart,
+    faMedal,
+    faHandHoldingUsd,
+    faPeace,
+    faSpaceShuttle,
+    faLeaf,
+    faPooStorm,
+    faRadiation,
+    faSkullCrossbones
+  ],
+  team: [
+    faGem,
+    faHeart,
+    faMedal,
+    faHandHoldingUsd,
+    faHatWizard,
+    faUserAstronaut,
+    faJedi,
+    faPooStorm,
+    faMask,
+    faBiohazard
+  ],
+  job: [
+    faGem,
+    faHeart,
+    faMedal,
+    faHandHoldingUsd,
+    faBrain,
+    faUserAstronaut,
+    faSave,
+    faPooStorm,
+    faPray,
+    faHouseDamage
+  ],
+  prizes: [
+    {
+      combination: [faGem, faGem, faGem],
+      prize: "1'000'000",
+      note: 'WIN WIN WIN'
+    },
+    {
+      combination: [faHeart, faHeart, faHeart],
+      prize: "729'000",
+      note: 'WIN WIN WIN'
+    },
+    {
+      combination: [faMedal, faMedal, faMedal],
+      prize: "512'000",
+      note: 'WIN WIN'
+    },
+    {
+      combination: [faHandHoldingUsd, faHandHoldingUsd, faHandHoldingUsd],
+      prize: "512'000",
+      note: 'WIN'
+    },
+    {
+      combination: [faPeace, faHatWizard, faBrain],
+      prize: "512'000",
+      note: 'WIN WIN'
+    },
+    {
+      combination: [faSpaceShuttle, faUserAstronaut, faUserAstronaut],
+      prize: "512'000",
+      note: 'WIN WIN'
+    },
+    {
+      combination: [faLeaf, faJedi, faBrain],
+      prize: "512'000",
+      note: 'WIN WIN'
+    }
+  ]
+})
+
+export const getters = {
+  getIcons(state) {
+    return state.icons
+  },
+  getCompany(state) {
+    return state.company
+  },
+  getTeam(state) {
+    return state.team
+  },
+  getJob(state) {
+    return state.job
+  },
+  getPrizes(state) {
+    return state.prizes
+  }
+}
+
+export const mutations = {
+  increment(state) {
+    state.counter++
+  }
+}
